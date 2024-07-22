4823aae5de1ac3e74c8b51d655465a31b02709b5
ire-design <irenemwikalii04@gmail.com>
2024-07-22 15:39:15 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function superbowlWin(record) {
+    
+    const win = record.find(entry => entry.result === "W");
+
+    return win ? win.year : undefined;
+}
+
+const record = [
+    { year: 2018, result: "L" },
+    { year: 2019, result: "W" }, 
+    { year: 2020, result: "L" }
+];
+
+console.log(superbowlWin(record)); 
